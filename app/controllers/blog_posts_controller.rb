7fe08844df83251225af63cbef931fda107c88e1
Patrick <patrickfeeneytamayo@gmail.com>
2025-01-27 18:21:10 +0000

    a

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,52 +1,52 @@
 class BlogPostsController < ApplicationController
-    before_action :authenticate_user!, except: [ :index, :show ]
-    before_action :set_blog_post, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_blog_post, only: [ :show, :edit, :update, :destroy]
 
-    def index
-        @blog_posts = BlogPost.all
+  def index
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+  end
+
+  def show
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @blog_post = BlogPost.new
-    end
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
 
-    def create
-        @blog_post = BlogPost.new(blog_post_params)
-        if @blog_post.save
-          redirect_to @blog_post
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def edit
-    end
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body, :published_at)
+  end
 
-    def update
-        if @blog_post.update(blog_post_params)
-          redirect_to @blog_post
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-      @blog_post.destroy
-      redirect_to root_path
-    end
-
-    private
-
-    def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
-    end
-
-    def set_blog_post
-      @blog_post = BlogPost.find(params[:id])
-    rescue
-        redirect_to root_path
-    end
+  def set_blog_post
+    user_signed_in? ? @blog_post = BlogPost.find(params[:id]) : @blog_post = BlogPost.published.find(params[:id])
+  rescue
+    redirect_to root_path
+  end
 end
